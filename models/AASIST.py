6034d574726f092323f6b36269cf512d6f4a28aa
yashpatil12345678 <yashhp050205@gmail.com>
2026-09-19 14:17:14 +0530

    Add GraphAttentionLayer class to AASIST.py

--- models/AASIST.py
+++ models/AASIST.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 from torch import Tensor
 
 
+
 class GraphAttentionLayer(nn.Module):
     def __init__(self, in_dim, out_dim, **kwargs):
         super().__init__()
